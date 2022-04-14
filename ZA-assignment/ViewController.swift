cff8156e19a18f6e55297d2d8b06e847f9512ce1
Nguyen Viet My <zzvietmyzz@gmail.com>
2022-04-14 21:10:17 +0700

    Initial Commit

---
+++ ZA-assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZA-assignment
+//
+//  Created by VinBrain on 14/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
